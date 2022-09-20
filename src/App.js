e5dfc8fe49aa7620b95e4216193aeb7c32210c06
Sadik Cavdar <sadik.cavdar@ecozum.teknik>
2022-09-20 16:10:02 +0300

    Components Create

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Counter from './components/Counter'
+import ChangeColor from './components/ChangeColor'
 
 function App() {
   return (
     <div className="App">
       Git Work
+      <Counter/>
+      <ChangeColor/>
     </div>
   );
 }
---
+++ src/components/ChangeColor.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ChangeColor() {
+  return (
+    <div>ChangeColor</div>
+  )
+}
+
+export default ChangeColor
