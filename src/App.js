bf02f078b16ab00ea81cff9a2efd421c8f93ac89
Sadik Cavdar <sadik.cavdar@ecozum.teknik>
2022-09-20 16:03:38 +0300

    Change color event is added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [isActive, setIsActive] = useState(false);
+
+  const changeColor = () => {
+    isActive === false ? setIsActive(true) : setIsActive(false)
+  }
   return (
-    <div className="App">
-      Git Work
+    <div className="App" style={{ backgroundColor: isActive ? "red" : "blue" }}>
+     
+     <div className='header'>
+        <h1>GİTWORK</h1>
+      </div>
+
+      <div className='changeColor'>
+        <button className='btn-changeColor' onClick={changeColor}>Change Color</button>
+      </div>
+      
     </div>
   );
 }
