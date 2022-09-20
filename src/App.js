c9ab8d00ae28ab7242e6f3af97527696c1cc45aa
Sadik Cavdar <sadik.cavdar@ecozum.teknik>
2022-09-20 16:00:42 +0300

    Counter added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [counter, setCounter] = useState(0);
+
+  const increaseCounter = () => {
+    setCounter(counter + 1)
+  }
+
+  const decreaseCounter = () => {
+    setCounter(counter - 1)
+  }
+
   return (
     <div className="App">
-      Git Work
+       <div className='header'>
+        <h1>GİTWORK</h1>
+      </div>
+
+      <div className='counter'>
+        <button onClick={increaseCounter}>+</button>
+        <h1>{counter}</h1>
+        <button onClick={decreaseCounter}>-</button>
+      </div>
+
     </div>
   );
 }
