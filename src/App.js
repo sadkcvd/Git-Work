8a71fa2901d81269f14d4e685db9379d2c8954aa
Sadik Cavdar <sadik.cavdar@ecozum.teknik>
2022-09-21 10:05:10 +0300

    Main is clear

--- src/App.js
+++ src/App.js
@@ -1,31 +1,16 @@
-import { useState } from 'react';
 import './App.css';
-import Counter from './components/Counter'
-import ChangeColor from './components/ChangeColor'
+
 
 function App() {
 
-  const [counter, setCounter] = useState(0);
-
-  const increaseCounter = () => {
-    setCounter(counter + 1)
-  }
-
-  const decreaseCounter = () => {
-    setCounter(counter - 1)
-  }
 
   return (
     <div className="App">
+
       <div className='header'>
         <h1>GİTWORK</h1>
       </div>
 
-      <div className='counter'>
-        <button onClick={increaseCounter}>+</button>
-        <h1>{counter}</h1>
-        <button onClick={decreaseCounter}>-</button>
-      </div>
     </div>
   );
 }
